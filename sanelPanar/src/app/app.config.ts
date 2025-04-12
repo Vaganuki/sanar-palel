91c8bf311f486bfbf06a54bc3464df4cddbcc042
Vaganuki <hugo.warnotte@gmail.com>
2025-04-12 11:44:14 +0200

    ajout provideHttpClient()

--- sanelPanar/src/app/app.config.ts
+++ sanelPanar/src/app/app.config.ts
@@ -7,11 +7,13 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import {providePrimeNG} from 'primeng/config';
 
 import Aura from '@primeng/themes/aura';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     providePrimeNG({
